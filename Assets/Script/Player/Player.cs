5e74522d67a46ef097266abd9c90d5b5fd8c269c
yeobi1124@gmail.com <lsy051229@hanyang.co.kr>
2024-09-19 17:07:41 +0900

    Update files

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
 
 public class Player : MonoBehaviour
 {
+    public int speed;
+    public int jumpPower;
+    float dir;
+    Rigidbody2D rigid;
+
+    Vector2 vecHorizion;
+    Vector2 vecVertical;
+
+    public bool isJump;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        isJump = false;
     }
 
     // Update is called once per frame
@@ -17,17 +29,28 @@ public class Player : MonoBehaviour
         
     }
 
+    private void FixedUpdate() {
+        //rigid.MovePosition(transform.position + speed * dir * Vector3.right * Time.fixedDeltaTime);
+        //rigid.velocity = speed * dir * Vector2.right;
+        rigid.velocity = vecHorizion + vecVertical;
+
+        if(isJump){
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isJump = false;
+        }
+    }
+
     void OnMove(InputValue value){
-        Debug.Log(value);
+        dir = value.Get<float>();
+        vecHorizion = speed * dir * Vector2.right;
     }
 
     void OnJump(InputValue value){
         Debug.Log("Jump");
-        Debug.Log(value);
+        isJump = true;
     }
 
     void OnMouse(InputValue value){
         Debug.Log("Mouse Click");
-        Debug.Log(value);
     }
 }
